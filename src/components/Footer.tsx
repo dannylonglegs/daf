9a2e6ece37b9d4669d58d4cc1ff8a2dc32522344
Daniel Fernandes <ddanfernan@gmail.com>
2026-02-08 17:53:50 -0500

    update footer links and icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ export const Footer = () => {
             <nav className="flex flex-row justify-between w-full md:w-auto text-2xl md:gap-x-6">
                 {/* <a href="">Email</a> */}
                 <Link to="/" className="hover:blur-[2px] transition-all">Home</Link>
-                <Link to="/contact" className="hover:blur-[2px] transition-all">Contact</Link>
-                <Link to="/linkedin" className="hover:blur-[2px] transition-all">LinkedIn</Link>
-                <Link to="/github" className="hover:blur-[2px] transition-all">GitHub</Link>
+                <Link to="mailto:ddanfernan@gmail.com" className="hover:blur-[2px] transition-all">Contact</Link>
+                <Link to="https://www.linkedin.com/in/daniel-fernandes-179bb2196/" className="hover:blur-[2px] transition-all">LinkedIn</Link>
+                {/* <Link to="/github" className="hover:blur-[2px] transition-all">GitHub</Link> */}
             </nav>
             <p className="hidden md:block my-auto">&copy; {new Date().getFullYear()}</p>
         </footer>
