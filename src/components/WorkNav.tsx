d1f0f1d4f3f17184541f675eee09d73e3a46553a
Daniel Fernandes <ddanfernan@gmail.com>
2026-08-20 17:51:48 -0400

    fix build error

--- src/components/WorkNav.tsx
+++ src/components/WorkNav.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { randomNumbers } from "../lib/functions";
 import { useState } from "react";
